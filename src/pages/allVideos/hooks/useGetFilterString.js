8bd05dbec0491c5fd038753a6b253b78f4280d07
Mitia Yakor <147729819+Molokorusmlg@users.noreply.github.com>
2025-01-25 23:03:03 +0300

    deploing

--- src/pages/allVideos/hooks/useGetFilterString.js
+++
@@ -1,6 +0,0 @@
-export const useGetFilterString = (filterRequerst) => {
-  if (!filterRequerst.length) return "";
-  return filterRequerst.length === 1
-    ? filterRequerst[0]
-    : filterRequerst.join("||");
-};
--- src/pages/allVideos/types/videosInterface.ts
+++
@@ -1,7 +0,0 @@
-export interface IVideo {
-  id: string;
-  img1: string;
-  title: string;
-  username: string;
-  avatar: string;
-}
